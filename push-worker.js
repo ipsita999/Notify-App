0e9f1a1cf4c7e3c01f102a952ea378354f0c21d4
Ipsita Rao <ipsita.rao@gmail.com>
2021-08-27 18:31:46 -0400

    change domain

--- push-worker.js
+++ push-worker.js
@@ -1,9 +1,9 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1629993102.1400263
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1630103365.246036
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://ibb.co/jZGb7sR',
   defaultTitle: 'Subscribe!',
-  defaultActionURL: 'https://boredomishere.netlify.app/',
+  defaultActionURL: 'https://boredomendshere.netlify.app/',
   appKey: 'KJlTEOG7SESw9wJQ9sH92g',
   token: 'MTpLSmxURU9HN1NFU3c5d0pROXNIOTJnOmExQ1FYLU51aDFGZGppVldfOUV0QTVBTjJqZjd2VFpUQ1FxX1NrZFU4OGM',
   vapidPublicKey: 'BJFkIY_uWs0825z_oaF_v_L6vCEnp6iBQ-rnDIurhLkyN8ZdLf0UVij41hv6Sqmg1Anz5aK1GuFk3i1IXw1tsvo='
